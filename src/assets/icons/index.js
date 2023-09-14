7970ea1fe1a18383292348a542a2d9fae4321622
Austin Nze <austinnze1000@gmail.com>
2023-09-14 20:26:25 +0100

    resolved issue preventing build

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,12 +1,23 @@
 import home from "./Home.png";
 import apple from "./apple.png";
 import logo from "./tv.png";
-import play from "./play.png";
+import play from "./Play.png";
 import search from "./Search.png";
 import tvshow from "./tv-show.png";
 import calendar from "./Calendar.png";
 import projector from "./movie-projector.png";
-import logout from './Logout.png'
-import imdb from './imdb-icon.png'
+import logout from "./Logout.png";
+import imdb from "./imdb-icon.png";
 
-export { apple, logo, play, search, home, tvshow, calendar, projector, logout,imdb };
+export {
+  apple,
+  logo,
+  play,
+  search,
+  home,
+  tvshow,
+  calendar,
+  projector,
+  logout,
+  imdb,
+};

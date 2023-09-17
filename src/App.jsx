e1131b6f9f7f966020c961e8e530e82f31adf427
Austin Nze <austinnze1000@gmail.com>
2023-09-17 01:25:48 +0100

    re-routed to avoid 404 error when trying to navigate to /movied/:id

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import {
-  createBrowserRouter,
-  Link,
-  NavLink,
-  Routes,
-  BrowserRouter,
-  Route,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Dashboard from "./components/Dashboard";
-// import Movies from "./pages/Movies";
 import Home from "./pages/Home";
-import RootLayout from "./layouts/RootLayout";
 import MovieDetails from "./components/MovieDetails";
 import Error from "./components/Error";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route path="*" element={<Error />} />
-      <Route index element={<Home />} />
-      {/* <Route path="movies" element={<Movies />} /> */}
-      <Route path="dashboard" element={<Dashboard />} />
-      <Route path="movies/:id" element={<MovieDetails />} />
-    </Route>
-  )
-);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="*" element={<Error />}></Route>
+        <Route path="dashboard" element={<Dashboard />}></Route>
+        <Route path="/movies/:id" element={<MovieDetails />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

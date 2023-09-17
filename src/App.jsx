8e313953be4efb1653410b81a2eb446cacd360d3
Austin Nze <austinnze1000@gmail.com>
2023-09-17 03:59:30 +0100

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./components/Dashboard";
 import Home from "./pages/Home";
-// import MovieDetails from "./components/MovieDetails";
+import MovieDetails from "./components/MovieDetails";
 import Error from "./components/Error";
 
 function App() {
